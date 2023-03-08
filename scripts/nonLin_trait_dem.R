bcdae503b4282ba05f46b7df85b67728f27702bd
radchukv <radchuk.victoria@gmail.com>
2023-03-08 12:58:33 +0100

    check whether multiplication by -5 and then division by 4 is needed in the lgistic regression

--- scripts/nonLin_trait_dem.R
+++ scripts/nonLin_trait_dem.R
@@ -131,6 +131,39 @@ for(i in 1:15){
 dev.off()
 
 
+## suing both scaled trait and scaled dem. rate
+## plot of scaled data
+trait_dem_Bothscale <- ggplot(tot_scale, aes(x = Trait_value, y = DemR_value)) +
+  geom_point() +
+  ggforce::facet_wrap_paginate(~ ID, ncol = 4,
+                               scales = 'free', nrow = 5, page = 1) +
+  geom_smooth(method = 'lm', se = FALSE, col = 'black') +
+  geom_smooth(col = 'chocolate', fill = 'darkorange2', linetype = 'dashed') +
+  geom_text(size = 1, data = abbrev_d,
+            aes(y = Inf, x = -Inf, label = label, vjust = 1,
+                hjust = 0, colour  = 3)) +
+  theme(legend.position = 'none') + xlab('Trait') +
+  ylab('Demographic rate')
+print(trait_dem_Bothscale)
+
+ggforce::n_pages(trait_dem_Bothscale)
+pdf('./output/output_nonL/DemRatevsTrait_Bothscaled.pdf')
+for(i in 1:15){
+  trait_dem_scale <- ggplot(tot_scale, aes(x = Trait_value, y = DemR_value)) +
+    geom_point() +
+    ggforce::facet_wrap_paginate(~ ID, ncol = 4,
+                                 scales = 'free', nrow = 5, page = i) +
+    geom_smooth(method = 'lm', se = FALSE, col = 'black') +
+    geom_smooth(col = 'chocolate', fill = 'darkorange2', linetype = 'dashed') +
+    geom_text(size = 1.5, data = abbrev_d,
+              aes(y = Inf, x = -Inf, label = label, vjust = 1,
+                  hjust = 0, colour  = 3)) +
+    theme(legend.position = 'none')
+  print(trait_dem_scale)
+}
+dev.off()
+
+
 ## plot of unscaled data
 trait_dem <- ggplot(tot_scale, aes(x = Trait_mean, y = Demog_rate_mean)) +
   geom_point() +
@@ -230,6 +263,71 @@ dev.off()
 
 ## so, the function does behave well, but under low betas it resembles linear.. and that is
 ## what we get with the data
+
+
+
+##I want to check whether weindeed need that -5 in front of beta and then -0.5 and 4 as a multiplier...
+fun_sigm <- function(beta, Trait_value, interc){
+  Dem_rate <- (1/(1+exp(beta*Trait_value))) + interc
+  return(Dem_rate)
+}
+
+fun_sigm(beta = 1, Trait_value = 0, interc = 0)
+## adjusting the explored par-rs to highlight the range tested in the model and the one
+## identified in the empirical sTrait
+Trait_v <- seq(-2, 2, by = 0.1)
+
+interc <- seq(-2, 2, by = 1)
+
+beta <- seq(-5, 5, by = 1)
+
+DemRate <- unlist(lapply(Trait_v, FUN = function(x){fun_sigm(beta = 1, interc = 0, Trait_value = x)}))
+plot(Trait_v, DemRate)
+
+
+## and now trying to apply the function across diff. intercept and beta values
+dat <- data.frame(interc = numeric(0), demRate = numeric(0), beta = numeric(0),
+                  Trait_v = numeric(0))
+for(i in interc){
+  for(j in beta){
+    newDat <- data.frame(interc = rep(i, length(Trait_v)), beta = rep(j, length(Trait_v)),
+                         Trait_v = Trait_v)
+    newDat$demRate <- unlist(lapply(Trait_v, FUN = function(x){fun_sigm(beta = j, interc = i, Trait_value = x)}))
+    dat <- rbind(dat, newDat)
+  }
+}
+
+## to colour the panels reflecting the par-r values inferred from the empiricla data
+## add a df
+dat_col <- data.frame(interc = rep(c(-1, 0, 1, 2), 2),
+                      beta = rep(c(-0.1, 0.1),
+                                 each = length(c(-1, 0, 1, 2))),
+                      col = 'blue', demRate = 0, Trait_v = 0)
+
+pdf('./output/output_nonL/SupplFig_ParSpace_Sigmoid_betaRows_IntercColumns_LargerBetaRange.pdf', height = 8, width = 6)
+ggplot(dat, aes(x = Trait_v, y = demRate)) +
+  geom_line() +
+  geom_rect(data = dat_col, aes(fill = col),
+            xmin = -Inf, xmax = Inf,
+            ymin = -Inf, ymax = Inf,
+            fill = 'grey', alpha = 0.4) +
+  facet_grid(beta ~ interc) +
+  theme_bw() + ylab('Demographic rate') +
+  xlab('Trait') +
+  theme(panel.grid.minor = element_blank(),
+        strip.background = element_rect(fill = 'white'))
+
+dev.off()
+
+dat$demR_prob <- exp(dat$demRate) / (1+ exp(dat$demRate))
+hist(dat$demR_prob)
+pdf('./output/output_nonL/explore_Sigmoid_betaRows_IntercColumns_ProbabScale_LargerBetaRange.pdf', height = 10, width = 8)
+ggplot(dat, aes(x = Trait_v, y = demR_prob)) +
+  geom_line() + facet_grid(beta ~ interc) +
+  theme_bw()
+dev.off()
+
+
 
 
 ## I should scale the traits to mean  = 0 and sd = 2,  ## so that it is closer to how things are simulated...
